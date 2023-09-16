0f4a62563db84a651e97f593fbfde3bab2bdc3ec
Ali George <george7ali@gmail.com>
2023-09-17 00:46:52 +0100

    ensured that the submit button on the todo form is seen and functional

--- src/dommanipulation.js
+++ src/dommanipulation.js
@@ -3,3 +3,10 @@ export function spreadTodoForm(){
         document.querySelector("#todoForm").style.display = "block";
     })
 }
+
+export function submitTodo(){
+    document.querySelector("#submitTodo").addEventListener("click", (event) => {
+        event.preventDefault();
+        alert("haaa...")
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import { render } from "./controlpanel";
-import { remove} from "./createTodo";
+
 import { spreadTodoForm, removebtn} from "./dommanipulation";
 import { submitTodo} from "./dommanipulation";
 spreadTodoForm();
-// display();
-render();
 submitTodo();
-removebtn();
