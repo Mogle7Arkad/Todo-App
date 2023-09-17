69ad2089c190cb6ecbaaa67736bf6295af77717a
Ali George <george7ali@gmail.com>
2023-09-17 01:59:45 +0100

    created the todo object

---
+++ src/createTodo.js
@@ -0,0 +1,8 @@
+let Todo = [];
+
+function TodoOb(title, details, dueDate, priority){
+    this.title = title;
+    this.details = details;
+    this.dueDate = dueDate;
+    this.priority = priority;
+}
