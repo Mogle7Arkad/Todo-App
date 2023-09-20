ee4a0db5244108109415c1d6ecc88bb73bf46975
Ali George <george7ali@gmail.com>
2023-09-20 21:00:08 +0100

    create a render function to enable the todo created to be displayed on the screen

--- src/createTodo.js
+++ src/createTodo.js
@@ -26,6 +26,45 @@ export function clearForm(){
     priority.value = "";
 }
 
+export function renderOnScreen(){
+    let todoScreen = document.querySelector("#todoListsDiv");
 
+    let itemInnerButtons = document.createElement("dive");
+    itemInnerButtons.classList.add("item-btns");
+    
+    todoScreen.innerHTML = "";
+    for (let i = 0; i < Todo.length; i++){
+        let todo = Todo[i];
+        let todoElement = document.createElement("li");
+        todoElement.classList.add("todo-item");
+        todoElement.innerHTML = `
+        <h3 class = "todo-title">${todo.title}</h3>
+        <h5 class = "todo-details">${todo.details}</h5>
+        <h5 class = "todo-date">${todo.dueDate}</h5>
+        <h4 class = "todo-priority">${todo.priority}</h4>
+        `;
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("delete-btn");
+        deleteBtn.innerHTML = "X";
+
+        let completedBtn = document.createElement("button");
+        completedBtn.classList.add("completed-btn");
+        completedBtn.innerHTML = "Completed";
+
+        deleteBtn.addEventListener("click", function(event){
+            event.preventDefault();
+            const item = event.target;
+            const todo = item.parentElement;
+            todo.remove();
+        })
+
+        todoElement.appendChild(completedBtn);
+        todoElement.appendChild(deleteBtn);
+        
+        todoScreen.appendChild(todoElement);
+    }
+    
+}
 
 
